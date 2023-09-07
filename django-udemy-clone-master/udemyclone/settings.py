f8f69c033d44643670954a142ed36bd3c729a735
harriszargar <harizzargar@gmail.com>
2023-09-07 18:24:05 +0530

    secretkey

--- django-udemy-clone-master/udemyclone/settings.py
+++ django-udemy-clone-master/udemyclone/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = 's3cr3tK3yF0rPr0duct10n!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
